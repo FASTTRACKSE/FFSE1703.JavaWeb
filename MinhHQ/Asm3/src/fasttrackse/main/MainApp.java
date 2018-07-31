038d193f04efd278a84a120cb94d2f58113fd53a
minhhm <minhhm3010@users.noreply.github.com>
2018-07-31 16:13:02 +0700

    update

--- MinhHQ/Asm3/src/fasttrackse/main/MainApp.java
+++ MinhHQ/Asm3/src/fasttrackse/main/MainApp.java
@@ -1,27 +1,26 @@
 package fasttrackse.main;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.Scanner;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import fasttrackse.entity.SinhVien;
 
+public class MainApp {
+	public static Scanner myScanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// Nạp file config spring từ applicationContext.xml
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		// Tạo bean từ spring containner
+		SinhVien sinhVien1 = context.getBean("sinhvien1", SinhVien.class);
+		System.out.println("Nhập tên sinh viên: ");
+		String Name = myScanner.nextLine();
+		sinhVien1.setHoTen(Name);
+		SinhVien sinhVien2 = context.getBean("sinhvien2", SinhVien.class);
+		System.out.println(sinhVien1.mangSachDiHoc());
+		System.out.println(sinhVien2.mangSachDiHoc());
 
- 
-public class MainApp{
- 
-       public static void main(String[] args) {
-              // Nạp file config spring từ applicationContext.xml
-              ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-             
-              // Tạo bean từ spring containner
-              SinhVien sinhVien1 = context.getBean("sinhvien1", SinhVien.class);
-      			sinhVien1.setHoTen("Hồ Quang Minh");
-      			SinhVien sinhVien2 = context.getBean("sinhvien2", SinhVien.class);
-      		
-      		System.out.println(sinhVien1.mangSachDiHoc());
-      		System.out.println(sinhVien2.mangSachDiHoc());
-      		
-      		context.close();
- 
-       }
- 
+		context.close();
+
+	}
+
 }

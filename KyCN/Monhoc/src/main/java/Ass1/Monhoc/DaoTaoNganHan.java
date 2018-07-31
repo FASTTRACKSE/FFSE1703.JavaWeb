5e1e70b72fc57c9b99997a07c9accc5479f342cb
Cngocky <35908359+Cngocky@users.noreply.github.com>
2018-07-31 15:35:28 +0700

    update

---
+++ KyCN/Monhoc/src/main/java/Ass1/Monhoc/DaoTaoNganHan.java
@@ -0,0 +1,7 @@
+package Ass1.Monhoc;
+
+public class DaoTaoNganHan implements HeDaoTao {
+	public String heDaoTao() {
+	return " Hệ đào tạo ngắn hạn 6 tháng giờ học hằng ngày từ 18h30 đến 20h";
+	}
+}

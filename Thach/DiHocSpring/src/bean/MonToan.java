a5fd4020f05ab2b07d617974f348712500fd9a0c
nguyenducthachffse1703011 <37531686+nguyenducthachffse1703011@users.noreply.github.com>
2018-08-01 15:07:29 +0700

    Update MonHoc Spring

---
+++ Thach/DiHocSpring/src/bean/MonToan.java
@@ -0,0 +1,7 @@
+package bean;
+
+public class MonToan implements MonHoc {
+	public String mangSachDiHoc() {
+		return "Mang sách Toán đi học";
+	}
+}
--- Thach/MonHocExample/src/main/StudentMain.java
+++ Thach/MonHocExample/src/main/StudentMain.java
@@ -1,58 +1,86 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bean.KhoaHocDaiHan;
+import bean.KhoaHocNganHan;
 import bean.MonHoa;
+import bean.MonHoc;
+import bean.MonLy;
 import bean.MonToan;
 import bean.Student;
 
 public class StudentMain {
-	public static Scanner myInput=new Scanner(System.in);
+	public static Scanner myInput = new Scanner(System.in);
 	public static Student objMon = new Student();
 	public static String ten;
+	public ArrayList<MonHoc> arrMonHoc = new ArrayList<>();
+
 	public static void main(String[] args) {
-		myMenu();
-		
+		chonKhoahoc();
+
 	}
-	public static void myMenu() {
+
+	public static void chonKhoahoc() {
 		while (true) {
 			System.out.println("Nhập Họ Và Tên Sinh Viên:");
 			ten = myInput.nextLine();
+			objMon.setHoTen(ten);
+			System.out.println("<======LỰA CHỌN KHÓA HỌC======>");
+			System.out.println("|| 1.Khóa học dài hạn            ");
+			System.out.println("|| 2.Khóa học ngắn hạn           ");
+			System.out.println("<===============================>");
+			System.out.println("      LỰA CHỌN CỦA BẠN        ");
+			int option = Integer.parseInt(myInput.nextLine());
+			if (option == 1) {
+				objMon.setKhoaHoc(new KhoaHocDaiHan());
+				myMenu();
+			} else {
+				objMon.setKhoaHoc(new KhoaHocNganHan());
+				myMenu();
+			}
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+
+			}
+		}
+	}
+
+	public static void myMenu() {
+		while (true) {
+
 			objMon.setHoTen(ten);
 			System.out.println("<======LỰA CHỌN MÔN HỌC======>");
 			System.out.println("|| 1.Toán                     ");
 			System.out.println("|| 2.Lý                       ");
 			System.out.println("|| 3.Hóa                      ");
-			System.out.println("<===============================>");
+			System.out.println("|| 4.in                       ");
+			System.out.println("<============================>");
 			System.out.println("      LỰA CHỌN CỦA BẠN        ");
-			int option = Integer.parseInt(myInput.nextLine());
+			int option = myInput.nextInt();
 			if (option == 1) {
-				toan();
+				objMon.setMonHoc(new MonToan());
 			} else if (option == 2) {
-				Ly();
+				objMon.setMonHoc(new MonLy());
 			} else if (option == 3) {
-				Hoa();
-			} 
-                try {
-					throw new Exception();
-				} catch (Exception e) {
-					
-				}
+				objMon.setMonHoc(new MonHoa());
+			} else {
+				in();
+			}
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+
 			}
 		}
-	public static void toan() {
-		myInput.nextLine();
-		objMon.setMonHoc(new MonToan()); 
-		objMon.mangSachDiHoc();
 	}
-	public static void Ly() {
-		myInput.nextLine();
-		objMon.setMonHoc(new MonHoa());
-		objMon.mangSachDiHoc();
-		}
-	public static void Hoa() {
-		myInput.nextLine();
-		objMon.setMonHoc(new MonHoa());
-		objMon.mangSachDiHoc();
+
+	public static void in() {
+		objMon.diHoc();
 	}
+
 }

981e2f157e29cd4701a6b68fa01245f0d20ce35f
tuhoviet <32056545+tuhoviet@users.noreply.github.com>
2018-08-07 14:56:04 +0700

    asm4

--- TuHV/QuanLySinhVien/src/fasttrackse/entity/HocKy3.java
+++ TuHV/QuanLySinhVien/src/fasttrackse/entity/HocKy3.java
@@ -5,32 +5,29 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HocKy1 implements HocKy {
-	public List<MonHoc> monCongNghe;
-	public List<MonHoc> monTiengAnh;
 
-	public HocKy1() {
+public class HocKy3 implements HocKy {
+	public List<MonHoc> monCongNghe;
+
+	public HocKy3() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 
-	public HocKy1(List<MonHoc> monCongNghe, List<MonHoc> monTiengAnh) {
+	public HocKy3(List<MonHoc> monCongNghe) {
 		super();
 		this.monCongNghe = monCongNghe;
-		this.monTiengAnh = monTiengAnh;
 	}
 
 	@Override
 	public String thongTinHocKy() {
-		String st = " Tên học kỳ: Học kỳ 1 \n";
+		String st = " Tên học kỳ: Học kỳ chuyên đề \n";
 		st += " Danh sách môn học: \n";
 		st += "  * Môn Công Nghê: \n";
 		for (MonHoc mH : monCongNghe) {
 			st += mH.thongTinMonHoc();
 		}
-		st += "  * Môn tiếng Anh: \n";
-		for (MonHoc mH : monTiengAnh) {
-			st += mH.thongTinMonHoc();
-		}
 		return st;
 	}
+
 }

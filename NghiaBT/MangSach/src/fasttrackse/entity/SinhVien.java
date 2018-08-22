06ea1b3768f742697095c0dea173156c72b1802f
Nghiabt <32056728+nghiabtn@users.noreply.github.com>
2018-08-22 14:30:52 +0700

    update

---
+++ NghiaBT/MangSach/src/fasttrackse/entity/SinhVien.java
@@ -0,0 +1,33 @@
+package fasttrackse.entity;
+
+public class SinhVien {
+	private String hoTen;
+	
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	
+	public SinhVien(String hoTen) {
+		super();
+		this.hoTen = hoTen;
+	}
+	
+	public String mangSachToanDiHoc() {
+		return "Bạn " + hoTen + " mang sách TOÁN đi học!";
+	}
+	
+	public String mangSachLyDiHoc() {
+		return "Bạn " + hoTen + " mang sách LÝ đi học!";
+	}
+	
+	public String mangSachHoaDiHoc() {
+		return "Bạn " + hoTen + " mang sách HÓA đi học!";
+	}
+
+	public String mangSachTiengAnhDiHoc() {
+		return "Bạn " + hoTen + " mang sách ENGLISH đi học!";
+	}
+}

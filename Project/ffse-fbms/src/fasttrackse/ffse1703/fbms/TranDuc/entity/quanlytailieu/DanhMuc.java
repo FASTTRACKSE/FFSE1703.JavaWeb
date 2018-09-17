713c3a4f5367769b9880780b2e44e5651a65d150
TranDuc <ffse1702057@st.fasttrack.edu.vn>
2018-09-17 14:40:30 +0700

    myHeader

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/TranDuc/entity/quanlytailieu/DanhMuc.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/TranDuc/entity/quanlytailieu/DanhMuc.java
@@ -4,5 +4,26 @@ public class DanhMuc {
 	String idDM;
 	String tenDM;
 	String maPhongBan;
+	public DanhMuc() {
+		
+	}
+	public String getIdDM() {
+		return idDM;
+	}
+	public void setIdDM(String idDM) {
+		this.idDM = idDM;
+	}
+	public String getTenDM() {
+		return tenDM;
+	}
+	public void setTenDM(String tenDM) {
+		this.tenDM = tenDM;
+	}
+	public String getMaPhongBan() {
+		return maPhongBan;
+	}
+	public void setMaPhongBan(String maPhongBan) {
+		this.maPhongBan = maPhongBan;
+	}
 	
 }
--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/TranDuc/entity/quanlytailieu/TaiLieu.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/TranDuc/entity/quanlytailieu/TaiLieu.java
@@ -8,5 +8,50 @@ public class TaiLieu {
 	String maPhongBan;
 	String link;
 	String trangthai;
+	public TaiLieu() {
+		
+	}
+	public String getMaTL() {
+		return maTL;
+	}
+	public void setMaTL(String maTL) {
+		this.maTL = maTL;
+	}
+	public String getTenDM() {
+		return tenDM;
+	}
+	public void setTenDM(String tenDM) {
+		this.tenDM = tenDM;
+	}
+	public String getIconTL() {
+		return iconTL;
+	}
+	public void setIconTL(String iconTL) {
+		this.iconTL = iconTL;
+	}
+	public String getTenTL() {
+		return tenTL;
+	}
+	public void setTenTL(String tenTL) {
+		this.tenTL = tenTL;
+	}
+	public String getMaPhongBan() {
+		return maPhongBan;
+	}
+	public void setMaPhongBan(String maPhongBan) {
+		this.maPhongBan = maPhongBan;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getTrangthai() {
+		return trangthai;
+	}
+	public void setTrangthai(String trangthai) {
+		this.trangthai = trangthai;
+	}
 	
 }

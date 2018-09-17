c7f3a18409c34bf3702c6d886be2531889f19965
minhhm <32055554+minhhm@users.noreply.github.com>
2018-09-17 21:27:33 +0700

    sua loi trung ten class

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/qlynhiemvuminhhq/QLyNhiemVuDao.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/qlynhiemvuminhhq/QLyNhiemVuDao.java
@@ -7,20 +7,20 @@ import fasttrackse.ffse1703.fbms.entity.qlynhiemvuminhhq.CongViec;
 
 
 
-public interface QLyNhiemVuDao {
-	public List<CongViec> findAll();
+// public interface QLyNhiemVuDao {
+// 	public List<CongViec> findAll();
 
-	public void addNew(CongViec cv);
+// 	public void addNew(CongViec cv);
 
-	public void update(CongViec cv);
+// 	public void update(CongViec cv);
 
-	public void delete(int ID);
+// 	public void delete(int ID);
 
-	public CongViec findByID(int ID);
+// 	public CongViec findByID(int ID);
 	
-	public List<CongViec> findAll(int iDisplayStart, int iDisplayLength, String sql);
+// 	public List<CongViec> findAll(int iDisplayStart, int iDisplayLength, String sql);
 	
-	public String getRecordsTotal();
+// 	public String getRecordsTotal();
 	
-	public String getRecordsFiltered(String sql);
-}
+// 	public String getRecordsFiltered(String sql);
+// }

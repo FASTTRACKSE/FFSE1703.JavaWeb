3a31c6c23d3bb9c7281f9230e0dd68631f542592
ronbi2810 <37531475+ronbi2810@users.noreply.github.com>
2018-09-18 11:27:20 +0700

    entity khach hang

---
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/service/mvpquanliduan/KhachHangServiceImpl.java
@@ -0,0 +1,42 @@
+package fasttrackse.ffse1703.fbms.service.mvpquanliduan;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fasttrackse.ffse1703.fbms.dao.mvpquanliduan.KhachHangDao;
+import fasttrackse.ffse1703.fbms.entity.mvpquanliduan.KhachHang;
+
+@Service
+public class KhachHangServiceImpl implements KhachHangService {
+	@Autowired 
+	KhachHangDao khachHangDao;
+	@Override
+	public void add(KhachHang khachHang) {
+		this.khachHangDao.add(khachHang);
+	}
+
+	@Override
+	public KhachHang getById(String id) {
+		return this.khachHangDao.getById(id);
+	}
+
+	@Override
+	public List<KhachHang> getAll() {
+		return this.khachHangDao.getAll();
+	}
+
+	@Override
+	public void update(KhachHang khachHang) {
+		this.khachHangDao.update(khachHang);
+		
+	}
+
+	@Override
+	public void delete(String id) {
+		this.khachHangDao.delete(id);
+		
+	}
+
+}

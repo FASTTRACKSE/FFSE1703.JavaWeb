126459bdb61d109313d68bf4adef8ba1e3187bad
tranthitam <FFSE1702029@st.fasttrack.edu.vn>
2018-09-19 17:24:03 +0700

    fix error

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/quanlynhansutt/QuanLyHoSoControllerTT.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/quanlynhansutt/QuanLyHoSoControllerTT.java
@@ -1,30 +1,25 @@
 package fasttrackse.ffse1703.fbms.controller.quanlynhansutt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import fasttrackse.ffse1703.fbms.service.quanlynhansutt.QuanLyHoSoServiceTT;
 
 @Controller
 @RequestMapping("/quanlyhoso/")
 public class QuanLyHoSoControllerTT {
-	@Autowired
-	private QuanLyHoSoServiceTT quanLyHoSoServiceTT;
-
-	public QuanLyHoSoServiceTT getQuanLyHoSoServiceTT() {
-		return quanLyHoSoServiceTT;
-	}
-
-	public void setQuanLyHoSoServiceTT(QuanLyHoSoServiceTT quanLyHoSoServiceTT) {
-		this.quanLyHoSoServiceTT = quanLyHoSoServiceTT;
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String viewHoSo(Model model) {
-		model.addAttribute("listHoSo", quanLyHoSoServiceTT.getAllHoSo());
-		return "QuanLyNhanSuTT/QuanLyHoSoTT/list";
-	}
+//	@Autowired
+//	private QuanLyHoSoServiceTT quanLyHoSoServiceTT;
+//
+//	public QuanLyHoSoServiceTT getQuanLyHoSoServiceTT() {
+//		return quanLyHoSoServiceTT;
+//	}
+//
+//	public void setQuanLyHoSoServiceTT(QuanLyHoSoServiceTT quanLyHoSoServiceTT) {
+//		this.quanLyHoSoServiceTT = quanLyHoSoServiceTT;
+//	}
+//
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String viewHoSo(Model model) {
+//		model.addAttribute("listHoSo", quanLyHoSoServiceTT.getAllHoSo());
+//		return "QuanLyNhanSuTT/QuanLyHoSoTT/list";
+//	}
 }

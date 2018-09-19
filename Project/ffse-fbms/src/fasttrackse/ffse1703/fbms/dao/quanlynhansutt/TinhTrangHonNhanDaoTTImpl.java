a036012f4367e444f78ebe4c7f2d05a183864045
TranDuc <ffse1702057@st.fasttrack.edu.vn>
2018-09-19 15:19:04 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/FASTTRACKSE/FFSE1703.JavaWeb""
    This reverts commit 66e48cf70b6434cbbfa26c4fcf8d5c88a87f94dd.

---
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/quanlynhansutt/TinhTrangHonNhanDaoTTImpl.java
@@ -0,0 +1,14 @@
+package fasttrackse.ffse1703.fbms.dao.quanlynhansutt;
+
+import java.util.List;
+
+import fasttrackse.ffse1703.fbms.entity.quanlynhansutt.TinhTrangHonNhanTT;
+
+public class TinhTrangHonNhanDaoTTImpl implements TinhTrangHonNhanDaoTT {
+	@Override
+	public List<TinhTrangHonNhanTT> getAllTinhTrangHonNhan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

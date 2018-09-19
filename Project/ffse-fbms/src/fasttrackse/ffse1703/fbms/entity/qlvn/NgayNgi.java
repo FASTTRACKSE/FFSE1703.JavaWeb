2ed2e91ed8588c365291fffdb404e94b5f9ec415
ffse1703004 <37531648+ffse1703004@users.noreply.github.com>
2018-09-19 10:08:10 +0700

    updte

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/entity/qlvn/NgayNgi.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/entity/qlvn/NgayNgi.java
@@ -1,5 +1,28 @@
 package fasttrackse.ffse1703.fbms.entity.qlvn;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ngay_nghi")
 public class NgayNgi {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_ngay_nghi")
+    private int id;
+	
+	
+	
+	@Column(name="so_ngay_da_nghi")
+	private int soNgayDaNghi;
+	
+	@Column(name="so_ngay_con_lai")
+	private int soNgayConLai;
+	
+	
 }

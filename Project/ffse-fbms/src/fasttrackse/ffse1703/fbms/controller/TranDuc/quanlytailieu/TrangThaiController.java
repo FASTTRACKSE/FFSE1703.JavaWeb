2bd94eaad23c2cc3e7fbb8f20ce47b8de80e227f
TranDuc <ffse1702057@st.fasttrack.edu.vn>
2018-09-20 17:16:06 +0700

    up

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/TranDuc/quanlytailieu/TrangThaiController.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/TranDuc/quanlytailieu/TrangThaiController.java
@@ -11,7 +11,6 @@ public class TrangThaiController {
 //	@Autowired
 //	private DanhMucService serviceDM;
 	
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String viewTrangThai(Model model) {
 		return "TranDuc-QuanLyTaiLieu/TrangThai/TrangThaiList";
--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/TranDuc/quanlytailieu/TaiLieuDaoImpl.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/TranDuc/quanlytailieu/TaiLieuDaoImpl.java
@@ -2,58 +2,89 @@ package fasttrackse.ffse1703.fbms.dao.TranDuc.quanlytailieu;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fasttrackse.ffse1703.fbms.entity.TranDuc.quanlytailieu.TaiLieu;
+
 @Repository
 public class TaiLieuDaoImpl implements TaiLieuDao {
 	@Autowired
-	private SessionFactory session;
-	
-	public void setSession(SessionFactory session) {
-		this.session = session;
+	private SessionFactory sessionFactory;
+
+	public void setSession(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<TaiLieu> listAll() {
-		return null;
+		Session session = this.sessionFactory.openSession();
+		List<TaiLieu> list = session.createQuery("from TaiLieu").list();
+		session.close();
+		return list;
 	}
 
 	@Override
 	public void addTL(TaiLieu tl) {
-		
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(tl);
+		tx.commit();
+		session.close();
 	}
 
 	@Override
 	public void updateTL(TaiLieu tl) {
-		
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.update(tl);
+		tx.commit();
+		session.close();
 	}
 
 	@Override
 	public void deleteTL(String maTL) {
-		
+		Session session = this.sessionFactory.openSession();
+		Transaction tl = session.beginTransaction();
+		session.delete(session.get(TaiLieu.class, maTL));
+		tl.commit();
+		session.close();
 	}
 
 	@Override
 	public TaiLieu getTLbyID(String maTL) {
-		return null;
+		Session session = this.sessionFactory.openSession();
+		TaiLieu tl = session.get(TaiLieu.class, maTL);
+		session.close();
+		return tl;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<TaiLieu> listAll(int start, int limit) {
-		return null;
+		Session session = this.sessionFactory.openSession();
+		Query sql = session.createQuery("from TaiLieu");
+		sql.setFirstResult(start);
+		sql.setMaxResults(limit);
+		List<TaiLieu> list = sql.getResultList();
+		session.close();
+		return list;
 	}
 
 	@Override
 	public String getRecordsTotal() {
-		return null;
-	}
-
-	@Override
-	public String getRecordsFiltered(String sql) {
-		return null;
+		Session session = this.sessionFactory.openSession();
+		String sql = "SELECT COUNT(*) FROM `tai_lieu`";
+		Query query = session.createSQLQuery(sql);
+		String recordsTotal = query.getSingleResult().toString();
+		session.close();
+		return recordsTotal;
 	}
 
 }

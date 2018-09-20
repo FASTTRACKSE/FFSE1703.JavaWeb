552dbfd74d4d95ce428e2389a18fabdaf1e47934
tuhoviet <32056545+tuhoviet@users.noreply.github.com>
2018-09-20 15:11:30 +0700

    update

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/quanlyduan/DomainTeam1Controller.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/controller/quanlyduan/DomainTeam1Controller.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import fasttrackse.ffse1703.fbms.entity.quanlyduan.DomainTeam1;
 import fasttrackse.ffse1703.fbms.service.quanlyduan.DomainTeam1Service;
 @Controller
--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/service/quanlyduan/DomainTeam1ServiceImpl.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/service/quanlyduan/DomainTeam1ServiceImpl.java
@@ -1,7 +1,5 @@
 package fasttrackse.ffse1703.fbms.service.quanlyduan;
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

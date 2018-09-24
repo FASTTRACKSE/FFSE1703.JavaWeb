0545fe2cc07d5a7795d4d6750b36a7ab9c945121
ThanhCL <thanhcl@gmail.com>
2018-09-24 14:15:58 +0700

    Revert "QLyNhiemVuzupn"
    This reverts commit 8545f37036f92a21261e0cf99ecee768c252eb50.

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/entity/qlnhiemvuzupn/NhanVienEntity.java
+++
@@ -1,5 +0,0 @@
-package fasttrackse.ffse1703.fbms.entity.qlnhiemvuzupn;
-
-public class NhanVienEntity {
-
-}
--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/service/qlnhiemvuzupn/QLyNhiemVuServiceImplzupn.java
+++
@@ -1,5 +0,0 @@
-package fasttrackse.ffse1703.fbms.service.qlnhiemvuzupn;
-
-public class QLyNhiemVuServiceImplzupn {
-
-}

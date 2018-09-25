4e6de7119f69ba15b24ea8659647aab4e4b2e7d5
ronbi2810 <37531475+ronbi2810@users.noreply.github.com>
2018-09-25 21:14:24 +0700

    CRUD PhanCongDuAn

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/entity/mvpquanliduan/Nhiemvu.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/entity/mvpquanliduan/Nhiemvu.java
@@ -1,5 +1,74 @@
 package fasttrackse.ffse1703.fbms.entity.mvpquanliduan;
 
-public class Nhiemvu {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import fasttrackse.ffse1703.fbms.entity.security.HoSoNhanVien;
+
+@Entity
+@Table(name = "qlda_role_assignment")
+public class Nhiemvu {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "id_project")
+	private int idProjects;
+	 @ManyToOne
+	 @JoinColumn(name="id_employee")
+	 private HoSoNhanVien hoSoNhanVien;
+	
+
+	
+	 @ManyToOne
+	 @JoinColumn(name = "id_role")
+	 private Roles roles;
+	private int status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdProjects() {
+		return idProjects;
+	}
+
+	public void setIdProjects(int idProjects) {
+		this.idProjects = idProjects;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public HoSoNhanVien getHoSoNhanVien() {
+		return hoSoNhanVien;
+	}
+
+	public void setHoSoNhanVien(HoSoNhanVien hoSoNhanVien) {
+		this.hoSoNhanVien = hoSoNhanVien;
+	}
+
+	public Roles getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Roles roles) {
+		this.roles = roles;
+	}
+
+	
 }

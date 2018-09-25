83448519b84694e49e8cbd49ad8caec2d2fe80b7
JsTuan <37531510+JsTuan@users.noreply.github.com>
2018-09-25 16:48:54 +0700

    crud nhan vien

--- Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/quantridanhgia/PhongNhanSuDAO.java
+++ Project/ffse-fbms/src/fasttrackse/ffse1703/fbms/dao/quantridanhgia/PhongNhanSuDAO.java
@@ -2,128 +2,43 @@ package fasttrackse.ffse1703.fbms.dao.quantridanhgia;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
 import fasttrackse.ffse1703.fbms.entity.quantridanhgia.DanhGiaBanThan;
 import fasttrackse.ffse1703.fbms.entity.quantridanhgia.KyDanhGia;
 import fasttrackse.ffse1703.fbms.entity.quantridanhgia.LichDanhGia;
 import fasttrackse.ffse1703.fbms.entity.quantridanhgia.PhanCongDanhGia;
 
-@Repository
-public class PhongNhanSuDAO {
+public interface PhongNhanSuDAO {
 
-	@Autowired
-	public SessionFactory sessionFactory;
+	public List<KyDanhGia> getListKyDanhGia();
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+	public void insertKyDanhGia(KyDanhGia kyDanhGia);
 
-	// CRUD kỳ đánh giá
-	@SuppressWarnings("unchecked")
-	public List<KyDanhGia> getListKyDanhGia() {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from KyDanhGia").list();
-	}
+	public void updateKyDanhGia(KyDanhGia kyDanhGia);
 
-	public void insertKyDanhGia(KyDanhGia kyDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		session.persist(kyDanhGia);
-	}
+	public KyDanhGia getKyDanhGia(int maKy);
 
-	public void updateKyDanhGia(KyDanhGia kyDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		session.update(kyDanhGia);
-	}
+	public void deleteKyDanhGia(KyDanhGia kyDanhGia);
 
-	public KyDanhGia getKyDanhGia(int maKy) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.get(KyDanhGia.class, maKy);
-	}
+	public List<Integer> getNhanVienPhongBan(String phongBan);
 
-	public void deleteKyDanhGia(KyDanhGia kyDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		session.update(kyDanhGia);
-	}
+	public List<LichDanhGia> getListLichDanhGia();
 
-	
-	// Get mã nhân viên phong ban
-	@SuppressWarnings("unchecked")
-	public List<Integer> getNhanVienPhongBan(String phongBan) {
-		Session session = sessionFactory.getCurrentSession();
-		String sql = "SELECT ma_nhan_vien FROM ho_so_nhan_vien WHERE ma_phong_ban = ? and ma_chuc_danh = 'NV'";
-		return session.createSQLQuery(sql).setParameter(1, phongBan).list();
-	}
+	public int checkLichDanhGia(LichDanhGia lichDanhGia);
 
-	
-	// Tạo phân công đánh giá
-	public void insertPhanCongDanhGia(List<PhanCongDanhGia> phanCong) {
-		Session session = sessionFactory.getCurrentSession();
-		for (PhanCongDanhGia x : phanCong) {
-			session.persist(x);
-		}
-	}
+	public void insertLichDanhGia(LichDanhGia lichDanhGia);
 
-	// CRUD lịch đánh giá
-	@SuppressWarnings("unchecked")
-	public List<LichDanhGia> getListLichDanhGia() {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from LichDanhGia").list();
-	}
+	public LichDanhGia getLichDanhGia(int id);
 
-	public void insertLichDanhGia(LichDanhGia lichDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		session.persist(lichDanhGia);
-	}
+	public void activeLichDanhGia(LichDanhGia lichDanhGia);
 
-	public LichDanhGia getLichDanhGia(int id) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.get(LichDanhGia.class, id);
-	}
+	public void insertPhanCongDanhGia(List<PhanCongDanhGia> pc);
 
-	
-	// Active lịch đánh giá
-	public void activeLichDanhGia(LichDanhGia lichDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		session.update(lichDanhGia);
-	}
+	public List<DanhGiaBanThan> getListDanhGiaBanThan();
 
-	// Check lịch đánh giá
-	public int checkLichDanhGia(LichDanhGia lichDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from LichDanhGia where kyDanhGia = :kyDG and phongBan = :phongBan")
-				.setParameter("kyDG", lichDanhGia.getKyDanhGia()).setParameter("phongBan", lichDanhGia.getPhongBan())
-				.list().size();
-	}
+	public List<DanhGiaBanThan> getListDanhGiaBanThanByKyDanhGia(String kyDanhGia);
 
-	// Get list đánh giá bản thân của toàn công ty
-	@SuppressWarnings("unchecked")
-	public List<DanhGiaBanThan> getListDanhGiaBanThan() {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from DanhGiaBanThan").list();
-	}
-	
-	// Get list đánh giá bản thân theo kỳ đánh giá
-	@SuppressWarnings("unchecked")
-	public List<DanhGiaBanThan> getListDanhGiaBanThanByKyDanhGia(String kyDanhGia) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from DanhGiaBanThan where kyDanhGia = :kyDanhGia").setParameter("kyDanhGia", kyDanhGia).list();
-	}
-	
-	// Get list đánh giá bản thân theo phòng ban
-	@SuppressWarnings("unchecked")
-	public List<DanhGiaBanThan> getListDanhGiaBanThanByPhongBan(String phongBan) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from DanhGiaBanThan where phongBan = :phongBan").setParameter("phongBan", phongBan).list();
-	}
-	
-	// Get list đánh giá bản thân theo kỳ đánh giá và phòng ban
-	@SuppressWarnings("unchecked")
-	public List<DanhGiaBanThan> getListDanhGiaBanThan(String kyDanhGia, String phongBan) {
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from DanhGiaBanThan where kyDanhGia = :kyDanhGia and phongBan = :phongBan").setParameter("kyDanhGia", kyDanhGia).setParameter("phongBan", phongBan).list();
-	}
+	public List<DanhGiaBanThan> getListDanhGiaBanThanByPhongBan(String phongBan);
+
+	public List<DanhGiaBanThan> getListDanhGiaBanThan(String kyDanhGia, String phongBan);
+
 }
